c39cc40a1bc76a9360c59b23f88dddcafe2b175f
Luigi Caradonna <luigi.caradonna@gmail.com>
2023-03-08 22:28:16 +0100

    Code refactoring

--- cs.js
+++ cs.js
@@ -8,11 +8,16 @@ chrome.runtime.onMessage.addListener(
 });
 
 // Container for the URLs list
-let tablist = document.createElement('div');
+let urls_list = document.createElement('div');
 // Modal Window
 let modal = document.createElement('div');
 // Overlay containing all the elements
 let overlay = document.createElement('div');
+
+// Initialize the container for the URL
+function initUrlsList() {
+  urls_list.setAttribute("class", 'tabsurlUrlsList');
+}
 
 // Initilizes the content of the modal window
 function initModal(tabs) {
@@ -22,32 +27,24 @@ function initModal(tabs) {
   `;
 
   // Contains the list of the URLs
-  let currentTabs = document.createDocumentFragment();
+  let current_tabs = document.createDocumentFragment();
 
-  tablist.textContent = '';
+  urls_list.textContent = '';
 
   // Loops through the tabs
   for (let tab of tabs) {
-    // If the tab has a URL
-    if (tab.url != '' && tab.url != undefined) {
-      let checkbox = document.createElement('input');
-      checkbox.setAttribute("type", "checkbox");
-      checkbox.setAttribute("id", tab.id);
-      // p element to contain a URL
-      let tabLink = document.createElement('p');
-  
-      // Adds the URL to the p element
-      tabLink.textContent = tab.url;
+    // If the tab has an URL
+    if (tab.url != undefined && tab.url != '') {
+      const url_entry = urlElement(tab.id, tab.url);
       // Adds the p element to the URLs container
-      currentTabs.appendChild(checkbox);
-      currentTabs.appendChild(tabLink);
+      current_tabs.appendChild(url_entry);
     }
   }
 
   // Adds the URLs to the div
-  tablist.appendChild(currentTabs);
+  urls_list.appendChild(current_tabs);
   // Add the div to the modal window
-  modal.appendChild(tablist);
+  modal.appendChild(urls_list);
 };
 
 // Initilizes the overlay
@@ -64,12 +61,44 @@ function destroy() {
   overlay.remove();
 }
 
+// Builds a URL entry
+function urlElement(id, url) {
+  // Container for the URL entry
+  const url_container = document.createElement('div');
+  // Checkbox to select the relative URL
+  const checkbox = document.createElement('input');
+  // p element to contain a URL
+  const tab_url = document.createElement('p');
+
+  // Setup the container
+  url_container.setAttribute("class", 'tabsurlContainer');
+
+  // Setup the checkbox, checked by default
+  checkbox.setAttribute("type", 'checkbox');
+  checkbox.setAttribute("id", id);
+  checkbox.setAttribute("class", 'tabsurlSelector');
+  checkbox.setAttribute("checked", true);
+
+  // Setup the paragraph containing the URL
+  tab_url.setAttribute("class", 'tabsurlTabUrl');
+  // Adds the URL to the p element
+  tab_url.textContent = url;
+
+  // Add the checkbox and the URL to the container
+  url_container.appendChild(checkbox);
+  url_container.appendChild(tab_url);
+
+  // return the container
+  return url_container;
+}
+
 // Initializes all the elements
 function init(tabs) {
   
   if(document.getElementById('tabsurlOverlay'))
     document.getElementById('tabsurlOverlay').remove();
   else {
+    initUrlsList();
     initModal(tabs);
     initOverlay();
   }
