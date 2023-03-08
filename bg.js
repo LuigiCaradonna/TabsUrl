def2be6a8d2339bd6c6439f948430051059020af
Luigi Caradonna <luigi.caradonna@gmail.com>
2023-03-08 16:50:19 +0100

    Added comments

--- bg.js
+++ bg.js
@@ -1,9 +1,12 @@
+// Listens for the click on the extension's button
 chrome.browserAction.onClicked.addListener(tab => {
+  // Gets the open tabs and sends them to the content script through a message
   getCurrentWindowTabs().then((tabs) => {
     chrome.tabs.sendMessage( tab.id, {data: tabs});
   });
 });
 
+// Returns the open tabs
 function getCurrentWindowTabs() {
   return browser.tabs.query({currentWindow: true});
 }
--- cs.js
+++ cs.js
@@ -1,3 +1,4 @@
+// Listens for a message from the backend script
 chrome.runtime.onMessage.addListener(
   function (request, sender) {
 
@@ -6,10 +7,14 @@ chrome.runtime.onMessage.addListener(
     }
 });
 
-let tablist;
-let modal;
-let overlay;
+// Container for the URLs list
+let tablist = document.createElement('div');
+// Modal Window
+let modal = document.createElement('div');
+// Overlay containing all the elements
+let overlay = document.createElement('div');
 
+// Initilizes the content of the modal window
 function initModal(tabs) {
   modal.setAttribute("id", 'tabsurlModal');
   modal.style.cssText=`
@@ -20,25 +25,32 @@ function initModal(tabs) {
     width: 80%;
   `;
   modal.innerHTML = `
-      <span class="close" style="font-size:24px;font-weight:bold;">&times;</span>
-      <p>Contenuto modal</p>
+      <span id="closeTabsUrlIcon" class="close" style="font-size:24px;font-weight:bold;cursor:pointer">&times;</span>
   `;
 
+  // Contains the list of the URLs
   let currentTabs = document.createDocumentFragment();
 
   tablist.textContent = '';
 
+  // Loops through the tabs
   for (let tab of tabs) {
+    // p element to contain a URL
     let tabLink = document.createElement('p');
 
+    // Adds the URL to the p element
     tabLink.textContent = tab.url;
+    // Adds the p element to the URLs container
     currentTabs.appendChild(tabLink);
   }
 
+  // Adds the URLs to the div
   tablist.appendChild(currentTabs);
+  // Add the div to the modal window
   modal.appendChild(tablist);
 };
 
+// Initilizes the overlay
 function initOverlay() {
   overlay.setAttribute("id", 'tabsurlOverlay');
   overlay.style.cssText=`
@@ -50,20 +62,20 @@ function initOverlay() {
     z-index: 9998;
     background: rgba(0, 0, 0, 0.4);
   `;
+  // Adds the content to the overlay
   overlay.appendChild(modal);
+  // Adds the overlay to the page
   document.body.appendChild(overlay);
 }
 
+// Removes the overlay from the page
 function destroy() {
   overlay.remove();
 }
 
+// Initializes all the elements
 function init(tabs) {
   
-  tablist = document.createElement('div');
-  modal = document.createElement('div');
-  overlay = document.createElement('div');
-
   if(document.getElementById('tabsurlOverlay'))
     document.getElementById('tabsurlOverlay').remove();
   else {
@@ -71,9 +83,17 @@ function init(tabs) {
     initOverlay();
   }
 
+  // Remove the overlay when ESC is pressed
   document.body.addEventListener('keyup',(e)=>{
     if (e.keyCode === 27)
       destroy();
+  });
+
+  // Remove the overlay when a click occurs outside of the modal or on the X icon
+  document.body.addEventListener("click", (e) => {
+    if (e.target.id == 'tabsurlOverlay' || e.target.id == 'closeTabsUrlIcon') {
+      destroy();
+    }
   });
   
 }
