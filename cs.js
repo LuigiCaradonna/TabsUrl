236908aa1fb18c3c0363612f487dd87646503e18
Luigi Caradonna <luigi.caradonna@gmail.com>
2023-03-09 12:24:22 +0100

    Implemented the  selection system for the urls

--- cs.js
+++ cs.js
@@ -7,6 +7,11 @@ chrome.runtime.onMessage.addListener(
     }
 });
 
+// This will contain the data of the open tabs
+let urls_list = {};
+// This will contain the checkboxes' ids of the selected URLs
+let urls_selected = [];
+
 // Modal Window
 let modal = document.createElement('div');
 // Overlay containing all the elements
@@ -15,9 +20,7 @@ let overlay = document.createElement('div');
 // Initilizes the content of the modal window
 function initModal(tabs) {
   modal.setAttribute("id", 'tabsurlModal');
-  modal.innerHTML = `
-      <span class="tabsurlCloseModal">&times;</span>
-  `;
+  modal.innerHTML = `<span class="tabsurlCloseModal">&times;</span>`;
 
   const modal_footer = document.createElement("div");
   modal_footer.setAttribute("id", 'tabsurlFooter');
@@ -31,7 +34,8 @@ function initModal(tabs) {
 
   table_list.setAttribute("id", 'tabsurlTable');
 
-  table_header_th_list.innerHTML = `<input type="checkbox">`;
+  table_header_th_list.setAttribute("colspan", 2);
+  table_header_th_list.innerHTML = `<input type="checkbox" checked="checked">`;
   table_header_tr_list.appendChild(table_header_th_list);
   table_header_list.appendChild(table_header_tr_list);
   table_list.appendChild(table_header_list);
@@ -41,7 +45,12 @@ function initModal(tabs) {
   for (let tab of tabs) {
     // If the tab has an URL
     if (tab.url != undefined && tab.url != '') {
-      const url_row = urlElement(tab.id, tab.url);
+      // Insert the tab's data into the urls list
+      urls_list[tab.id] = {title: tab.title, url: tab.url};
+      // Add the tab's id to the selected urls (all the urls are selected by default)
+      urls_selected.push(tab.id);
+      // Create the table row for this tab
+      const url_row = urlElement(tab.id, tab.title);
       // Adds the table row to the URLs table
       table_body_list.appendChild(url_row);
     }
@@ -68,19 +77,21 @@ function destroy() {
 }
 
 // Builds a table row
-function urlElement(id, url) {
+function urlElement(id, title) {
   const tr = document.createElement("tr");
   const td_check = document.createElement("td");
   const td_title = document.createElement("td");
   const checkbox = document.createElement('input');
 
+  // Setup the checkbox, checked by default
   checkbox.setAttribute("type", 'checkbox');
   checkbox.setAttribute("id", id);
+  // Class used with the click listener
   checkbox.setAttribute("class", 'tabsurlSelector');
   checkbox.setAttribute("checked", true);
 
   td_check.appendChild(checkbox);
-  td_title.textContent = url;
+  td_title.textContent = title;
 
   tr.appendChild(td_check);
   tr.appendChild(td_title);
@@ -90,13 +101,12 @@ function urlElement(id, url) {
 
 // Initializes all the elements
 function init(tabs) {
-  
+  // Remove the overlay if exists
   if(document.getElementById('tabsurlOverlay'))
     document.getElementById('tabsurlOverlay').remove();
-  else {
-    initModal(tabs);
-    initOverlay();
-  }
+  
+  initModal(tabs);
+  initOverlay();
 
   // Remove the overlay when ESC is pressed
   document.body.addEventListener('keyup',(e)=>{
@@ -104,10 +114,19 @@ function init(tabs) {
       destroy();
   });
 
-  // Remove the overlay when a click occurs outside of the modal or on the X icon
+  // Mouse click event listener
   document.body.addEventListener("click", (e) => {
+    // Remove the overlay when a click occurs outside of the modal or on the close icon
     if (e.target.id == 'tabsurlOverlay' || e.target.classList.contains('tabsurlCloseModal')) {
       destroy();
+    }
+    else if (e.target.classList.contains('tabsurlSelector')) {
+      // Get the checkbox
+      const cb = document.getElementById(e.target.id);
+      // If the checkbox has been found
+      if (cb)
+        // Add or remove the id from the list of the selected URLs according to the checkbox status
+        cb.checked == true ? urls_selected.push(e.target.id) : urls_selected.pop(e.target.id);
     }
   });
   
