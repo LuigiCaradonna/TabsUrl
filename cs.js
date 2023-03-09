6439c33cb47f8ec4493a4772e215eef157e2bb3a
Luigi Caradonna <luigi.caradonna@gmail.com>
2023-03-10 00:15:27 +0100

    Code refactoring

--- cs.js
+++ cs.js
@@ -8,9 +8,9 @@ chrome.runtime.onMessage.addListener(
 });
 
 // This will contain the data of the open tabs
-let urls_list = [];
+let tabs_data_list = [];
 // This will contain the checkboxes' ids of the selected URLs
-let urls_selected = [];
+let tabs_selected = [];
 
 // Modal Window
 let modal = document.createElement('div');
@@ -26,6 +26,7 @@ function initModal(tabs) {
   modal_footer.setAttribute("id", 'tabsurlFooter');
   modal_footer.innerHTML = `<button id="tabsurlSaveTxt" class="tabsurlBtn">Save txt</button>`;
 
+  // Building the table containing the tabs list
   const table_list = document.createElement("table");
   const table_header_list = document.createElement("thead");
   const table_header_tr_list = document.createElement("tr");
@@ -45,10 +46,10 @@ function initModal(tabs) {
   for (let tab of tabs) {
     // If the tab has an URL
     if (tab.url != undefined && tab.url != '') {
-      // Insert the tab's data into the urls list
-      urls_list.push({id: tab.id, title: tab.title, url: tab.url});
+      // Insert the tab's data into the list
+      tabs_data_list.push({id: tab.id, title: tab.title, url: tab.url});
       // Add the tab's id to the selected urls (all the urls are selected by default)
-      urls_selected.push(tab.id);
+      tabs_selected.push(tab.id);
       // Create the table row for this tab
       const url_row = urlElement(tab.id, tab.title);
       // Adds the table row to the URLs table
@@ -56,7 +57,7 @@ function initModal(tabs) {
     }
   }
 
-  console.log(urls_selected);
+  console.log(tabs_selected);
 
   // Add table to the modal window
   modal.appendChild(table_list);
@@ -102,34 +103,34 @@ function urlElement(id, title) {
 }
 
 // Builds a list containing only the selected URLs to save
-function urlsListToSave() {
+function tabsListToSave() {
   // Data of the selected tabs to be saved
-  let urls_to_save = {};
+  let tabs_to_save = {};
   // Text to save into the file
   let content = '';
 
   let key_int = 0;
 
-  console.log('urls selected: ' + urls_selected);
+  console.log('tabs selected: ' + tabs_selected);
   // Loop through all the tabs data
-  for (const [key, value] of Object.entries(urls_list)) {
-    console.log('url_list key: ' + value.id + ': ' + value.title);
+  for (const [key, value] of Object.entries(tabs_data_list)) {
+    console.log('tabs_list key: ' + value.id + ': ' + value.title);
     key_int = parseInt(value.id);
     // If the current tab is among the selected ones
-    if ( urls_selected.includes(key_int) ) {
+    if ( tabs_selected.includes(key_int) ) {
       console.log('Adding tab: ' + key_int);
       // Add the tab data
-      urls_to_save[value.id] = {id: value.id, title: value.title, url: value.url};
+      tabs_to_save[key_int] = {id: value.id, title: value.title, url: value.url};
     }
   }
 
-  console.log('urls to save: ' + urls_to_save);
+  console.log('tabs to save: ' + tabs_to_save);
 
   // Get the string to be saved
-  content = buildTextList(urls_to_save);
+  content = buildTextList(tabs_to_save);
 
-  // Save to file
-  saveToFile(content);
+  // Return the text to save
+  return content;
 }
 
 // Builds and returns the string to be saved
@@ -144,7 +145,8 @@ function buildTextList(list) {
 }
 
 // Saves the given string into a text file
-function saveToFile(content) {
+function saveToFile() {
+  const content = tabsListToSave();
   const textToBlob = new Blob([content], { type: 'text/plain' });
   const sFileName = 'tabsurl.txt';
 
@@ -193,21 +195,21 @@ function init(tabs) {
         // Add or remove the id from the list of the selected URLs according to the checkbox status
         if (cb.checked == true) {
           console.log('Add id: ' + e.target.id);
-          urls_selected.push(parseInt(e.target.id));
-          console.log(urls_selected);
+          tabs_selected.push(parseInt(e.target.id));
+          console.log(tabs_selected);
         }
         else {
           console.log('Remove id: ' + e.target.id);
-          const index = urls_selected.indexOf(parseInt(e.target.id));
+          const index = tabs_selected.indexOf(parseInt(e.target.id));
           if (index > -1) { // only splice array when item is found
-            urls_selected.splice(index, 1); // 2nd parameter means remove one item only
+            tabs_selected.splice(index, 1); // 2nd parameter means remove one item only
           }
-          console.log(urls_selected);
+          console.log(tabs_selected);
         }
       }
     }
     else if (e.target.id == 'tabsurlSaveTxt') {
-      urlsListToSave();
+      saveToFile();
     }
   }); 
 }
